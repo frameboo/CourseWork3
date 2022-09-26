1342c7452930d08e9c119cc00ffee8f85fbc30ab
frame <frame.boo@gmail.com>
2022-09-26 14:32:36 +0300

    update app file

--- run.py
+++ run.py
@@ -1,19 +1,5 @@
-import loggers
-import logging
+from confapp import create_app
 
-from flask import Flask
-from app.posts.views import posts_blueprint
-from app.api.views import api_blueprint
-
-
-app = Flask(__name__)
-
-loggers.create_logger()
-logger = logging.getLogger("basic")
-
-
-app.register_blueprint(posts_blueprint)
-app.register_blueprint(api_blueprint)
-
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app = create_app()
+    app.run()
